56f6daa7f8712b30bbe62e86d0f5fd9d5e04e101
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2023-01-13 11:49:27 -0400

    project creation and configuration.

---
+++ src/Components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home () {
+  return (
+    <div>
+      <h1 className='font-bold text-md text-blue100_dark bg-gray-400'>
+        Home page hola
+      </h1>
+      <div className='bg-blue-500'>
+        <p>Hola</p>
+      </div>
+
+    </div>
+  )
+}
+
+export { Home }
